de39d01b7ef99bf350f1c44b42ba3aad453c0e5b
Katherine Obregon <a20181823@pucp.edu.pe>
2020-11-03 10:53:21 -0500

    le agregue getter y setter a prueba3

--- src/Prueba3.java
+++ src/Prueba3.java
@@ -1,4 +1,20 @@
 public class Prueba3 {
     private String apellido;
     private String codigo;
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
 }
